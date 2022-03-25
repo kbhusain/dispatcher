3612e85a08e81d0ed4a0176c74573d7dc7464e4f
husainun <kbhusain@gmail.com>
2022-03-25 11:58:58 -0500

    Modified action.js to include print div command

--- r2p/static/src/actions.js
+++ r2p/static/src/actions.js
@@ -81,3 +81,29 @@ function showRequestdetailsForPerson(pid, url, divname) {
 }
 
 
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+}
+
+function printDiv(divID) {
+    //Get the HTML of div
+    var divElements = document.getElementById(divID).innerHTML;
+    //Get the HTML of whole page
+    var oldPage = document.body.innerHTML;
+
+    //Reset the page's HTML with div's HTML only
+    document.body.innerHTML = 
+       "<html><head><title></title></head><body>" + 
+              divElements + "</body>";
+
+    //Print Page
+    window.print();
+
+    //Restore orignal HTML
+    document.body.innerHTML = oldPage;
+          
+}
